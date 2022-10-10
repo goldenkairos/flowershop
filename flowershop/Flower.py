0260b68f35d2825e65403ee80ba81f6524e918cb
Minh <minh.seikel@gmail.com>
2022-10-10 09:50:21 -0700

    creating Flower class and default flowers

---
+++ flowershop/Flower.py
@@ -0,0 +1,6 @@
+
+
+class Flower:
+    def __init__(self,type,color):
+        self.type = type
+        self.color = color
---
+++ main.py
@@ -0,0 +1,9 @@
+from flowershop.Flower import Flower
+
+red_rose = Flower("roses","red")
+white_rose = Flower("roses","rose")
+yellow_sunflowers = Flower("sunflowers","yellow")
+yellow_tulips = Flower("tulip", "yellow")
+blue_tulips =Flower("tulips", "blue")
+
+
